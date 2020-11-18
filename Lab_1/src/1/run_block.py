d75198b5b6b23481fe1892155ce88d4206e8ec78
giannis-k <ioanniskanaropoulos@gmail.com>
2020-11-18 21:50:22 +0200

    second plot

--- Lab_1/src/1/run_block.py
+++ Lab_1/src/1/run_block.py
@@ -1,27 +1,38 @@
-#!/bin/python3
+#!/usr/bin/python3
 import sys
 import os
 from os import path
 import subprocess
 import statistics
 import shutil
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 exec=["phods_data_reuse"]
 block=[1,2,4,8,16]
 if not(path.isdir("metrics/")):
     exit()
-#
-# os.mkdir("metrics")
+if not path.isdir("plots/"):
+    os.mkdir("plots")
+
+data = []
+bl = []
+
 for e in exec:
     f=open("metrics/"+e+".txt",'a')
     for b in block:
         times=[]
+        tmp = ([])
         for i in range(0,10):
             p = subprocess.Popen("./"+e+".out "+str(b), stdout=subprocess.PIPE, shell=True)
             (output, err) = p.communicate()
             p_status = p.wait()
             output = int(output)
             times.append(output)
+            data.append([output])
+            bl.append(b)
 
         mx=max(times)
         mn=min(times)
@@ -34,3 +45,10 @@ for e in exec:
         f.write("min: " + str(mn) +"\n")
         f.write("avg: " + str(avg) +"\n")
     f.close()
+
+data = tuple(data)
+data = np.array(data)
+data = pd.DataFrame(data, columns=["time(us)"])
+data['block size'] = bl
+pl = sns.catplot(x="block size", y="time(us)", kind="box", data=data)
+pl.savefig("plots/3.png")
