da51e0495cf144c65c1b5f26464b8dc4dbc2cc4d
manos <arapidismanolis@gmail.com>
2020-11-20 20:49:12 +0200

    added 3d plot

--- Lab_1/src/1/run_block_xy.py
+++ Lab_1/src/1/run_block_xy.py
@@ -6,17 +6,27 @@ import subprocess
 import statistics
 import shutil
 
+from mpl_toolkits import mplot3d
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 exec=["phods_data_reuse"]
 
 block_x=[1, 2, 3, 4, 6, 8, 9, 12, 16, 18, 24, 36, 48, 72, 144]
 block_y=[1, 2, 4, 8, 11, 16, 22, 44, 88, 176]
+
+dictx=dict()
+
+
 if not(path.isdir("metrics/")):
     exit()
-#
+
 # os.mkdir("metrics")
 for e in exec:
     f=open("metrics/"+e+".txt",'a')
     for bx in block_x:
+        dictx[bx]={}
         for by in block_y:
             times=[]
             for i in range(0,10):
@@ -32,6 +42,7 @@ for e in exec:
             mn=min(times)
             avg=statistics.mean(times)
 
+            dictx[bx].update({by:avg})
 
             f.write("\n")
             f.write("For block size x: " + str(bx) +" block size y: "+str(by)+"\n")
@@ -39,3 +50,29 @@ for e in exec:
             f.write("min: " + str(mn) +"\n")
             f.write("avg: " + str(avg) +"\n")
     f.close()
+
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+
+x_ticks=np.arange(0,len(block_x),1)
+y_ticks=np.arange(0,len(block_y),1)
+
+
+ax.set_xlim(0, len(x_ticks)-0.5)
+ax.set_ylim(0, len(y_ticks)-0.5)
+ax.xaxis.set_ticks(x_ticks)
+ax.set_xticklabels(block_x, rotation=0)
+ax.yaxis.set_ticks(y_ticks)
+ax.set_yticklabels(block_y, rotation=0)
+
+ax.set_xlabel("Bx Size",fontsize=12)
+ax.set_ylabel("By Size",fontsize=12)
+fig.suptitle("Average Time", fontsize=16,fontweight='bold')
+
+for x in range(0,15):
+    for y in range(0,10):
+        ax.scatter(x_ticks[x], y_ticks[y], dictx[block_x[x]][block_y[y]], color='red')
+
+# plt.savefig("test.png",bbox_inches="tight")
+plt.show()
