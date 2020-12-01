51ed8e23ac321a4821f55c652203475886ebb3b6
manos <arapidismanolis@gmail.com>
2020-12-01 21:08:38 +0200

    added plots

---
+++ Lab_2/src/dijsktra/plot.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+import glob
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+
+
+
+files=glob.glob("metrics/*")
+# print(files)
+
+configuration = list(map(lambda x: x.split(".")[0],list(map(lambda x: x.split("/")[1] ,files))))
+
+# print(configuration)
+
+x=[]
+y=[]
+
+for f in files:
+    fp = open(f,"r")
+    for i, line in enumerate(fp):
+        # print(line)
+        if i==0 :
+            x.append(int(line))
+        else:
+            tokens = line.split()
+            # print(tokens)
+            if tokens[1]=="KB":
+                y.append(float(tokens[0]))
+            else :
+                y.append(float(tokens[0])*1024)
+            break
+    fp.close()
+
+# print(configuration)
+# print(x)
+# print(y)
+
+colours = ListedColormap(['r','b','g'])
+values = [0, 1, 2]
+
+fig = plt.figure()
+ax = fig.gca()
+ax.set_xlabel("Memory accesses")
+ax.set_ylabel("Memory footprint (KB)")
+p=plt.scatter(x,y,c=values,cmap=colours)
+plt.grid()
+plt.legend(handles=p.legend_elements()[0], labels=configuration)
+plt.savefig("plot.png")
