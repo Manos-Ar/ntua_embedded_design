f93cba119895737f4d2c6d7ead45957fa79884f4
giannis-k <ioanniskanaropoulos@gmail.com>
2020-12-14 20:10:36 +0200

    host

--- Lab_3/src/ex2.c
+++ Lab_3/src/ex2.c
@@ -3,58 +3,65 @@
 #include <unistd.h>
 #include <termios.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 
 int main (int argc, char **argv){
 
-    int fd, i;
-    char input[64], output[64];
-    if (argc<2)
-        printf("Error. Invalid numder of arguments");
+    int fd;
+    struct termios options;
+    char input[64], output[64], *device;
 
-    char *device;
+    if (argc<2) {
+        printf("Error. Invalid numder of arguments\n");
+        return 1;
+    }
 
-    device = (char*) malloc(sizeof(argv[1]));
     device = argv[1];
-
     printf("device: %s\n", device);
 
-    fd = open(device, O_RDWR | O_NOCTTY | O_NONBLOCK);
+    printf("Please give a string to send to guest:\n");
+    fgets(input, 64, stdin);
 
+    fd = open(device, O_RDWR | O_NOCTTY | O_NONBLOCK);
     if(fd == -1) {
-         printf( "failed to open port\n" ); 
+         printf("Failed to open port\n");
+         return 1;
     }
+    fcntl(fd, F_SETFL, 0);
 
     printf("fd : %d\n", fd);
-    char c;
 
-    for(i=0; i<64 ; i++){
-        c = getchar();
-        input[i]=c;
-        if (c == '\n')
-            break;
+    if(tcgetattr(fd, &options) < 0) {
+        printf("Couldn't the information for the terminal associated with %s\n", device);
+        return 1;
     }
 
-    printf("input: %ssize: %d\n", input, i);
+    cfmakeraw(&options);
+    options.c_lflag |= ICANON;
+    options.c_cflag |= (CLOCAL | CREAD);
+    options.c_cflag &= ~CSTOPB;
+    options.c_cc[VMIN] = 5;
+    options.c_cc[VTIME] = 255;
 
-    int wr = write(fd, input, i);
-    printf("wd : %d\n" , wr);
+    if(cfsetispeed(&options, B9600) < 0 || cfsetospeed(&options, B9600) < 0) {
+        printf("Couldn't set communication speed\n");
+        return 1;
+    }
 
-    // if(wr < 0)
-    //     {
-    //         printf("error write\n");
-    //         exit(-1);
-    //     }
+    if(tcsetattr(fd, TCSANOW, &options) < 0) {
+        printf("Couldn't apply communication settings\n");
+        return 1;
+    }
 
-    // close(fd);
+	tcflush(fd, TCIOFLUSH);
+
+	write(fd, input, 64);
+
+    while (read(fd, output, 64) <= 0);
+    printf("The most frequent character is \"%c\" and it appeared %d times.\n", output[0], output[2]);
     
-    
-    int rd = -1;    
-    while( rd < 0)
-        rd=read(fd, output, 4);
-    printf("rd : %d\n" , rd);
-    printf("output: %s\n", output);
-
-
     close(fd);
+    return 0;
 }
